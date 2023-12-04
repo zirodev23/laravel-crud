5a1aeb5a025a8fad1eebbca35d42bc22baeb1b8b
zirodev <zirodev23@zirodev23.com>
2023-12-04 11:41:05 +0200

    npm install; add app.js and app.css via vite

--- public/custom.js
+++ public/custom.js
@@ -1,32 +1,34 @@
 const button = document.querySelector('#submitOrder');
 
-button.addEventListener('click', function(e){
-    e.preventDefault();
-    const data = new FormData(e.target.closest('form'));
-    const url = "/orders";
+if (button){
+    button.addEventListener('click', function(e){
+        e.preventDefault();
+        const data = new FormData(e.target.closest('form'));
+        const url = "/orders";
 
-    fetch(url, {
-        method : "POST",
-        body: data,
-        // -- or --
-        // body : JSON.stringify({
-            // user : document.getElementById('user').value,
-            // ...
-        // })
-    }).then(
-        response => {
-            console.log(response);
-            return response.text() // .json(), etc.
-        }
-        // same as function(response) {return response.text();}
-    ).then(
-        html => {
-            let rowtext = JSON.parse(html).html;
-            const tr = document.createElement("tr");
-            tr.innerHTML = rowtext;
-            document.querySelector("tbody").appendChild(tr);
-        }
-    );
+        fetch(url, {
+            method : "POST",
+            body: data,
+            // -- or --
+            // body : JSON.stringify({
+                // user : document.getElementById('user').value,
+                // ...
+            // })
+        }).then(
+            response => {
+                console.log(response);
+                return response.text() // .json(), etc.
+            }
+            // same as function(response) {return response.text();}
+        ).then(
+            html => {
+                let rowtext = JSON.parse(html).html;
+                const tr = document.createElement("tr");
+                tr.innerHTML = rowtext;
+                document.querySelector("tbody").appendChild(tr);
+            }
+        );
 
 
-});
+    });
+}
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>{{ $title }}</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body>
     <nav class="navbar bg-body-tertiary">
